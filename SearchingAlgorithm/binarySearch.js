416c11a1792b51031f797cc475a1b1ba18977687
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-01-16 20:58:51 +0545

    Refactored code

--- SearchingAlgorithm/binarySearch.js
+++ SearchingAlgorithm/binarySearch.js
@@ -17,4 +17,18 @@ function binarySearch(arr, elem) {
     return -1;
 }
 
-console.log(binarySearch([2, 4], 3));
+
+// Refactored Version
+function binarySearch(arr, elem) {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+    while(arr[middle] !== elem && start <= end) {
+        if(elem < arr[middle]) end = middle - 1;
+        else start = middle + 1;
+        middle = Math.floor((start + end) / 2);
+    }
+    return arr[middle] === elem ? middle : -1;
+}
+
+binarySearch([2,5,6,9,13,15,28,30], 103)
