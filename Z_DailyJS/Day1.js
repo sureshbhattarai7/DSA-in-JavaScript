745bcdffc8d3718e0b2c5d6d4fa05df30173bae4
Nischal <baniyanisal12@gmail.com>
2024-04-26 20:58:17 +0545

    second commit

--- Z_DailyJS/Day1.js
+++ Z_DailyJS/Day1.js
@@ -9,3 +9,20 @@ function SecondClass(){
 }
 
 MainClass(SecondClass)
+
+function Details(name, address, contact, email){
+    this.name = name;
+    this.address = address;
+    this.contact = contact;
+    this.email = email;
+}
+
+Object.prototype.flashdetails = function(){
+    console.log(`
+    WELCOME
+    Name: ${this.name}
+    Address: ${this.address}
+    Contact: ${this.contact}
+    Email: ${this.email}
+    `);
+}
